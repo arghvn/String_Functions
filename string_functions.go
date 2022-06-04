35de7cd541e804714cf26e69479d4d3108f1a22a
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-04 11:35:01 +0430

    comments

--- string_functions.go
+++ string_functions.go
@@ -1,13 +1,23 @@
 package main
 
+//The standard library’s strings package provides many useful string-related functions.
+//Here are some examples to give we a sense of the package.
+
 import (
 	"fmt"
 	s "strings"
 )
 
+//We alias fmt.Println to a shorter name as we’ll use it a lot below.
+
 var p = fmt.Println
 
 func main() {
+
+	//Here’s a sample of the functions available in strings.
+	//Since these are functions from the package, not methods on the
+	//string object itself, we need pass the string in question as the first
+	// argument to the function. You can find more functions in the strings package docs.
 
 	p("Contains:  ", s.Contains("test", "es"))
 	p("Count:     ", s.Count("test", "t"))
@@ -22,3 +32,17 @@ func main() {
 	p("ToLower:   ", s.ToLower("TEST"))
 	p("ToUpper:   ", s.ToUpper("test"))
 }
+
+//output :
+//Contains:   true
+//Count:      2
+//HasPrefix:  true
+//HasSuffix:  true
+//Index:      1
+//Join:       a-b
+//Repeat:     aaaaa
+//Replace:    f00
+//Replace:    f0o
+//Split:      [a b c d e]
+//ToLower:    test
+//ToUpper:    TEST
